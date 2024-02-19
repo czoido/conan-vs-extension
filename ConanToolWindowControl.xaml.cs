ee438d54cf7c79cf94455b8f7d8be84a02408e2b
czoido <mrgalleta@gmail.com>
2024-02-19 15:08:14 +0100

    wip

--- ConanToolWindowControl.xaml.cs
+++ ConanToolWindowControl.xaml.cs
@@ -503,98 +503,10 @@ class ConanApplication(ConanFile):
 
         }
 
-        private string getProfileName(string vcConfigName)
-        {
-            return vcConfigName.Replace("|", "_");
-        }
-
-        private string getConanArch(string platform)
-        {
-            var archMap = new Dictionary<string, string>();
-            archMap["x64"] = "x86_64";
-            archMap["Win32"] = "x86";
-            archMap["ARM64"] = "armv8";
-            return archMap[platform];
-        }
-
-        private string getConanCompilerVersion(string platformToolset)
-        {
-            var msvcVersionMap = new Dictionary<string, string>();
-            msvcVersionMap["v143"] = "193";
-            msvcVersionMap["v142"] = "192";
-            msvcVersionMap["v141"] = "191";
-            return msvcVersionMap[platformToolset];
-        }
-
-        private string getConanCppstd(string languageStandard)
-        {
-            // https://learn.microsoft.com/en-us/cpp/build/reference/std-specify-language-standard-version?view=msvc-170
-
-            if (languageStandard.ToLower().Contains("default"))
-            {
-                return "14";
-            }
-
-            List<string> cppStdValues = new List<string>() { "14", "17", "20", "23" };
-
-            foreach (string cppStdValue in cppStdValues)
-            {
-                if (languageStandard.Contains(cppStdValue))
-                {
-                    return cppStdValue;
-                }
-            }
-            return "null";
-        }
-
         private void ShowPackages_Click(object sender, RoutedEventArgs e)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
-            try
-            {
-                if (_dte != null && _dte.Solution != null && _dte.Solution.Projects != null)
-                {
-                    foreach (Project project in _dte.Solution.Projects)
-                    {
-                        if (project.Object is VCProject vcProject)
-                        {
-                            string projectDirectory = System.IO.Path.GetDirectoryName(project.FullName);
-                            string conanProjectDirectory = System.IO.Path.Combine(projectDirectory, ".conan");
-                            MessageBox.Show(string.Join("\n", GetConandataRequirements(projectDirectory)));
-                            if (!Directory.Exists(conanProjectDirectory))
-                            {
-                                Directory.CreateDirectory(conanProjectDirectory);
-                            }
 
-                            foreach (VCConfiguration vcConfig in (IEnumerable)vcProject.Configurations)
-                            {
-                                string profileName = getProfileName(vcConfig.Name);
-                                string profilePath = System.IO.Path.Combine(conanProjectDirectory, profileName);
-
-                                if (!File.Exists(profilePath))
-                                {
-                                    string toolset = vcConfig.Evaluate("$(PlatformToolset)").ToString();
-                                    string compilerVersion = getConanCompilerVersion(toolset);
-                                    string arch = getConanArch(vcConfig.Evaluate("$(PlatformName)").ToString());
-                                    IVCRulePropertyStorage generalRule = vcConfig.Rules.Item("ConfigurationGeneral") as IVCRulePropertyStorage;
-                                    string languageStandard = generalRule == null ? null : generalRule.GetEvaluatedPropertyValue("LanguageStandard");
-                                    string cppStd = getConanCppstd(languageStandard);
-                                    string buildType = vcConfig.ConfigurationName;
-                                    string profileContent = $"[settings]\narch={arch}\nbuild_type={buildType}\ncompiler=msvc\ncompiler.cppstd={cppStd}\ncompiler.runtime=dynamic\n" +
-                                        $"compiler.runtime_type={buildType}\ncompiler.version={compilerVersion}\nos=Windows";
-                                    File.WriteAllText(profilePath, profileContent);
-                                }
-                            }
-                        }
-                    }
-
-                    MessageBox.Show($"Generated profiles for actual project.", "Conan profiles generated", MessageBoxButton.OK, MessageBoxImage.Information);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"There was a problem generating the file: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
         }
 
         private async Task UpdateJsonDataAsync()
